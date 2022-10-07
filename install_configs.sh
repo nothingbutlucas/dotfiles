4077e9419ede261ac99eb06066bd1d78c9c1ba4d
lucas <lucasblancodev@gmail.com>
2022-10-06 22:46:22 -0300

    Added half-support to all distros, more and better prints to the user

--- install_configs.sh
+++ install_configs.sh
@@ -1,54 +1,39 @@
 #!/usr/bin/env bash
 
-grey=$(tput setaf 8)
-white=$(tput setaf 7)
-
-distro=$(lsb_release -is)
-
-if [[ ${distro,} = "fedora" || ${distro,} = "rhel" ]]; then
-    package="dnf"
-elif [[ ${distro,} = "debian" || ${distro,} = "ubuntu" ]]; then
-    package="apt"
-else
-    echo "$system"
-    echo -e "No se ha podido reconocer el sistema :("
-    exit 1
-fi
-
 function plugins_zsh(){
-    echo -e "Vamos a crear la carpeta para los plugins de la zsh"
+    echo -e "[+] Vamos a crear la carpeta para los plugins de la zsh"
     sudo mkdir -p /usr/share/zsh-plugins/
-    echo -e "Me descargo los plugins de la zsh"
     cd /usr/share/zsh-plugins/
-    echo -e "Voy a necesitar permisos de sudo para poder descargar los plugins de la zsh en /usr/share/zsh-plugins/"
-    sudo wget -b -t 5 "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh"
-    sudo wget -b -t 5 "https://raw.githubusercontent.com/zsh-users/zsh-autosuggestions/master/zsh-autosuggestions.zsh"
-    sudo wget -b -t 5 "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/jsontools/jsontools.plugin.zsh"
+    sleep 0.4
+    echo -e "[s] Voy a necesitar permisos de sudo para poder descargar los plugins de la zsh en /usr/share/zsh-plugins/"
+    sudo wget --nocache -b -t 5 "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh"
+    sudo wget --nocache -b -t 5 "https://raw.githubusercontent.com/zsh-users/zsh-autosuggestions/master/zsh-autosuggestions.zsh"
+    sudo wget --nocache -b -t 5 "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/jsontools/jsontools.plugin.zsh"
     sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
     cd -
 }
 
 function zsh(){
-    echo "Instalando zsh..."
-    sleep 0.5
+    echo "[s] Instalando zsh..."
     command="sudo $package install zsh -y"
+    echo -e "${grey}$ ${command}${white}"
+    sleep 0.5
     eval $command
 }
 
 function powerlevel10k(){
-    echo "Instalando powerlevel10k..."
+    echo "[+] Instalando powerlevel10k..."
     sleep 0.5
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
     echo "Ahora instala la powerlevel10k con el comando p10k configure"
 }
 
 function mpv_youtube_dl(){
-    echo -e "Instalando mpv"
+    echo -e "[s] Instalando mpv y youtube-dl..."
+    command="sudo $package install mpv youtube-dl -y"
+    echo -e "${grey}$ ${command}${white}"
     sleep 0.5
-    sudo $package install mpv -y
-    echo -e "Instalando youtube-dl"
-    sleep 0.5
-    sudo $package install youtube-dl -y
+    eval $command
 }
 
 function backup(){
@@ -94,23 +79,28 @@ function dotfiles(){
 
 function tmux(){
     command="sudo $package install tmux -y"
-    echo -e "Instalando tmux"
-    sleep 0.5
+    echo -e "[+]Instalando tmux"
     echo -e "${grey}$ ${command}${white}"
+    sleep 0.5
     eval $command
 }
 
 function salir(){
-    echo -e "Saliendo..."
-    sleep 0.5
+    echo -e "[x] Saliendo..."
+    sleep 0.2
     exit 0
 }
 
 function main(){
+    PS3="Elegi el programa que quieras instalar: "
+    if [[ $package = "none" ]]; then
+        programs='powerlevel10k plugins_zsh dotfiles salir'
+    else
+        programs='zsh powerlevel10k mpv_youtube_dl dotfiles tmux plugins_zsh salir'
+    fi
     while [[ 1 -eq 1 ]]; do
-        PS3="Elegi el programa que quieras instalar: "
-        select program in "zsh" "powerlevel10k" "mpv_youtube-dl" "dotfiles" "tmux" "plugins_zsh" "salir"; do
-            echo -e "Has seleccionado $program \n"
+        select program in $programs; do
+            echo -e "\n[+] Has seleccionado $program \n"
             sleep 0.5
             $program
             break
@@ -118,5 +108,20 @@ function main(){
     done
         exit 0
 }
+
+grey=$(tput setaf 8)
+white=$(tput setaf 7)
+
+distro=$(lsb_release -is)
+
+if [[ ${distro,} = "fedora" || ${distro,} = "rhel" ]]; then
+    package="dnf"
+elif [[ ${distro,} = "debian" || ${distro,} = "ubuntu" ]]; then
+    package="apt"
+else
+    package="none"
+    echo "La distribución $system aún tiene soporte al 100%"
+    exit 1
+fi
 
 main
