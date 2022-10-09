7559d52d0846f5e8a6be24defffc77a16e8642bc
lucas <lucasblancodev@gmail.com>
2022-10-08 21:12:34 -0300

    Take the music to another file and added a music lias

--- zshrc/alias.sh
+++ zshrc/alias.sh
@@ -11,20 +11,10 @@ alias edit_aliases="cd ${HOME}/dotfiles/ && ls -la"
 
 alias ttyclock='tty-clock -c -C 6 -t -B'
 
-# Music
-
-alias lofi='mpv --no-video https://youtu.be/jfKfPfyJRdk'
-alias deadmau5='mpv --no-video https://youtu.be/0DJ-mEAEnrM'
-alias ochentas='mpv --no-video https://youtu.be/e_Z1CawQIvo'
-alias the_clash='mpv --no-video https://youtu.be/l0Q8z1w0KGY'
-alias youtube_search='BROWSER=w3m ddgr -x -w youtube.com'
-alias jajacker_lofi='mpv --no-video https://youtu.be/QifY_BtOxR8'
-alias the_runaways='mpv --no-video https://youtu.be/cn_5WwhB_yA'
-alias ameli_lens='mpv --no-video https://youtu.be/9OtQpDypvBY'
-alias fito_tercer_mundo='mpv --no-video https://youtu.be/92bu97IgE2E'
-alias hack_net='mpv --no-video https://youtu.be/BEimFoWRXCw'
-alias mr_s01='mpv --no-video https://youtu.be/hicayypkI7g'
-
 # Security
 
 alias reverse_shell_ps="ps aux | grep /bin/bash/-i"
+
+# Music
+
+alias music='cat ${HOME}/dotfiles/zshrc/music.sh | sed "s/=/ /g" | awk "{print \$2}"'
