9fa05401380321375695def95641942991f60ced
lucas blanco <lucasblancodev@gmail.com>
2022-11-07 09:47:30 -0300

    Added more music

--- zshrc/music.sh
+++ zshrc/music.sh
@@ -9,6 +9,7 @@ alias the_white_stripes='mpv --no-video "https://youtube.com/playlist?list=PLMEZ
 alias ameli_lens='mpv --no-video https://youtu.be/9OtQpDypvBY'
 alias fito_tercer_mundo='mpv --no-video https://youtu.be/92bu97IgE2E'
 alias to_sexy='mpv --no-video https://youtu.be/P5mtclwloEQ'
+alias bad_michel='mpv --no-video "https://youtube.com/playlist?list=PL08865B6758432FE8"'
 alias hack_net='mpv --no-video https://youtu.be/BEimFoWRXCw'
 alias mr_s01='mpv --no-video https://youtu.be/hicayypkI7g'
 alias mr_s03='mpv --no-video https://youtu.be/P0sAYxS03i0'
