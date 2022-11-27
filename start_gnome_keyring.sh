5d22477a880fa5ee0cdce3475e2cbd51f3c9d3be
lucas <lucasblancodev@gmail.com>
2022-11-27 12:53:04 -0300

    Added script to start gnome keyring

--- start_gnome_keyring.sh
+++ start_gnome_keyring.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
-export $(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg)
+eval $(/usr/bin/gnome-keyring-daemon -s --components=gpg,pkcs11,secrets,ssh)
+export $(gnome-keyring-daemon -r --components=pkcs11,secrets,ssh,gpg)
 dbus-update-activation-environment --systemd DISPLAY
 
 exit 0
