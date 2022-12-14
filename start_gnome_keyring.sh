330b5dcb7a1af8e2a84c350494ccd06f41e8bc52
lucas <lucasblancodev@gmail.com>
2022-12-14 12:54:37 -0300

    Removed exit from script

--- start_gnome_keyring.sh
+++ start_gnome_keyring.sh
@@ -5,4 +5,4 @@ export $(gnome-keyring-daemon -r --components=pkcs11,secrets,ssh,gpg)
 # dbus-update-activation-environment --systemd DISPLAY
 export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
 
-exit 0
+# exit 0
