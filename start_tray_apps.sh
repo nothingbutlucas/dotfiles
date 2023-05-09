515437055d916794d9b2896418fdb8865dfb7f50
lucas <lucasblancodev@gmail.com>
2023-05-09 18:04:24 -0300

    My user, my tray apps

--- start_tray_apps.sh
+++ start_tray_apps.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
-if [ $(ps aux | grep solaar | wc -l) -eq 1 ]; then
+if [ $(ps aux | grep $USER | grep solaar | wc -l) -eq 1 ]; then
 	solaar --window=hide &
 	disown
 fi
 
-if [ $(ps aux | grep copyq | wc -l) -eq 1 ]; then
+if [ $(ps aux | grep $USER | grep copyq | wc -l) -eq 1 ]; then
 	copyq &
 	disown
 fi
 
-if [ $(ps aux | grep openrgb | wc -l) -eq 1 ]; then
+if [ $(ps aux | grep $USER | grep openrgb | wc -l) -eq 1 ]; then
 	openrgb --startminimized --profile angry &
 	disown
 fi
