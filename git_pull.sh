86904251116dd04670d8c2529f690271486cda68
lucas <lucasblancodev@gmail.com>
2023-05-31 21:25:09 -0300

    Git pull script

--- git_pull.sh
+++ git_pull.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-# eval $(/usr/bin/gnome-keyring-daemon --replace --components=gpg,pkcs11,secrets,ssh)
 export $(gnome-keyring-daemon -r --components=pkcs11,secrets,ssh,gpg)
-# dbus-update-activation-environment --systemd DISPLAY
 export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
-
-# exit 0
+git pull
+exit 0
