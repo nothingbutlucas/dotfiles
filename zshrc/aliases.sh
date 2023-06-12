a2de8ca6f385f5c8295808a0da9a133b81ebc6b9
lucas blanco <lucasblancodev@gmail.com>
2023-06-12 14:02:47 -0300

    Mouse jiggler alias

--- zshrc/aliases.sh
+++ zshrc/aliases.sh
@@ -17,6 +17,7 @@ alias ttyclock='tty-clock -c -C 6 -t -B'
 alias reverse_shell_ps="ps aux | grep /bin/bash/-i"
 alias block_incoming_ip="sudo iptables -A INPUT -s $1 -j DROP || echo 'Provide an IP address as argument'"
 alias block_outgoing_ip="sudo iptables -A OUTPUT -d $1 -j DROP || echo 'Provide an IP address as argument'"
+alias mouse_jiggler="while true; do xdotool mousemove_relative 1 1; sleep 1; xdotool mousemove_relative -- -1 -1; sleep 1; done"
 
 # Music
 
