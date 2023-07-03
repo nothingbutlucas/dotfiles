908a853766c2fc9f2929e3739a7e0730092d230e
user <example@mail.com>
2023-07-03 12:00:52 -0300

    Maybe there is not warp

--- polybar/warp.sh
+++ polybar/warp.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Verify if warp-cli exists
+
+if ! command -v warp-cli &>/dev/null; then
+	echo ""
+	exit
+fi
+
 warp_status=$(warp-cli status | grep "Status" | awk '{print $3}')
 
 if [[ $warp_status == "Disconnected." ]]; then
