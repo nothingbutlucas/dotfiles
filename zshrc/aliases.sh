7d4e9f33d5f25a874acc94f1fc2c4adcbb216286
lucas blanco <lucasblancodev@gmail.com>
2023-09-19 09:32:08 -0300

    New aliases

--- zshrc/aliases.sh
+++ zshrc/aliases.sh
@@ -3,10 +3,11 @@
 alias ..="cd .."
 alias cd..="cd .."
 alias c="bat"
-alias ls="lsd --hyperlink=auto"
+alias ls="lsd --hyperlink=auto --sort time"
 alias g="git"
 alias edit_aliases="cd ${HOME}/dotfiles/ && ls -la"
 alias which_key="xev | grep -A2 --line-buffered '^KeyRelease' | sed -n '/keycode /s/^.*keycode \([0-9]*\).* (.*, \(.*\)).*$/\1 \2/p'" # https://askubuntu.com/questions/411419/how-do-i-check-which-key-is-pressed
+alias gdiff="git difftool --no-symlinks --dir-diff"
 
 # Fast tty-clock configuration
 
@@ -26,3 +27,9 @@ alias music='cat ${HOME}/dotfiles/zshrc/music.sh | sed "s/=/ /g" | awk "{print \
 # unetbootin
 
 alias unetbootin='sudo QT_X11_NO_MITSHM=1 /usr/bin/unetbootin'
+
+# Misc
+
+alias is_shutdown_scheduled="cat /run/systemd/shutdown/scheduled | awk -F '=' '/USEC/{print $2}' | awk '{print int($1/1000000)}' | xargs -I{} date -d @{}"
+alias day="date +%A-%d-%m-%y"
+alias sound="play -n synth 0.15 sine 500"
