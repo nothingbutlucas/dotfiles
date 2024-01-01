229b09e5f0c99afb871be73ea9d234fdeef2ccd4
nothingbutlucas <lucasblancodev@gmail.com>
2024-01-01 14:05:18 -0300

    refactor(lsd command): Si lsd no existe redirecciono a /bin/ls

--- zshrc/plugins.sh
+++ zshrc/plugins.sh
@@ -1,6 +1,6 @@
 # Plugins
 
-FILES_STR=$(ls /usr/share/zsh-plugins/)
+FILES_STR=$(/bin/ls /usr/share/zsh-plugins/)
 FILES=($(echo $FILES_STR | tr '\n' ' '))
 
 source /usr/share/zsh-plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
