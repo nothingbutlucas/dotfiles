1dbde30b3bb36ecc10a246725c5cd3e152b264a7
nothingbutlucas <lucasblancodev@gmail.com>
2024-01-03 16:15:45 -0300

    fix(ls command): redirect its actually an error

--- zshrc/aliases.sh
+++ zshrc/aliases.sh
@@ -3,7 +3,7 @@
 alias ..="cd .."
 alias cd..="cd .."
 alias c="bat"
-alias ls="lsd --hyperlink=auto --sort time 2>/dev/null || /bin/ls --hyperlink"
+alias ls="lsd --hyperlink=auto --sort time"
 alias g="git"
 alias edit_aliases="cd ${HOME}/dotfiles/ && ls -la"
 alias which_key="xev | grep -A2 --line-buffered '^KeyRelease' | sed -n '/keycode /s/^.*keycode \([0-9]*\).* (.*, \(.*\)).*$/\1 \2/p'" # https://askubuntu.com/questions/411419/how-do-i-check-which-key-is-pressed
