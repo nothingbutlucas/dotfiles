8d86e66a02a6a87cd59d719df8175146cfaf036b
nothingbutlucas <lucasblancodev@gmail.com>
2024-01-05 22:39:02 -0300

    feat(polybar - Weather): New polybar weather script

--- polybar/weather.sh
+++ polybar/weather.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-weather=$(curl -s "wttr.in/$CITY?m&format=%t+%C+☔%p&lang=es" || echo "No weather info available")
+FILE=/tmp/weather.txt
+
+if [[ ! -f $FILE ]] || [[ $(find $FILE -mmin +60 -print -quit) ]]; then
+ weather=$(curl -s -w "%{http_code}" "wttr.in/$CITY?m&format=%t+%C+☔%p&lang=es" || echo "No weather info available")
+ http_status=${weather: -3}
+ weather=${weather::-3}
+ if [[ $http_status == "200" ]]; then
+   echo "$weather" > $FILE
+ fi
+fi
+
+weather=$(cat $FILE || echo "No weather info available")
 
 if [[ "$weather" == "Unknown"* ]]; then
 	echo ""
 else
 	echo "$weather"
-
 fi
+
