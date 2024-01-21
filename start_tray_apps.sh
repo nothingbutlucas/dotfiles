436b4ffef9288916aa5418819187dab0a586bab3
nothingbutlucas <lucasblancodev@gmail.com>
2024-01-21 10:30:06 -0300

    feat(tray_apps): Updated tray apps

--- start_tray_apps.sh
+++ start_tray_apps.sh
@@ -1,16 +1,11 @@
 #!/bin/bash -e
 
-if [ $(ps aux | grep $USER | grep solaar | wc -l) -eq 1 ]; then
+if [ $(pgrep -c solaar) -eq 0 ]; then
 	solaar --window=hide &
 	disown
 fi
 
-if [ $(ps aux | grep $USER | grep copyq | wc -l) -eq 1 ]; then
-	copyq &
-	disown
-fi
-
-if [ $(ps aux | grep $USER | grep openrgb | wc -l) -eq 1 ]; then
-	openrgb --startminimized --profile angry &
-	disown
+if [ $(pgrep -c opensnitch-ui) -eq 0 ]; then
+  opensnitch-ui &
+  disown
 fi
