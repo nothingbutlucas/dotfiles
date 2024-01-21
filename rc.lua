d98b82f2e141adafdb75f1f717cb199865f845c5
nothingbutlucas <lucasblancodev@gmail.com>
2024-01-21 10:29:37 -0300

    refactor(rc.lua): Deleted lots of dead code and comments

--- rc.lua
+++ rc.lua
@@ -1,5 +1,3 @@
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
 
 -- Standard awesome library
@@ -14,20 +12,16 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
--- Enable volume Control
--- local volume_widget = require("awesome-wm-widgets.volume-widget.volume")
 require("modules/create_class")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
 
 -- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
 	naughty.notify({
 		preset = naughty.config.presets.critical,
-		title = "Oops, there were errors during startup!",
+		title = "Uh, la cagaste con algo...",
 		text = awesome.startup_errors,
 	})
 end
@@ -44,60 +38,37 @@ do
 
 		naughty.notify({
 			preset = naughty.config.presets.critical,
-			title = "Oops, an error happened!",
+			title = "Embarradisimo!",
 			text = tostring(err),
 		})
 		in_error = false
 	end)
 end
--- }}}
 
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
+
 -- Capture user home directory
 local home = os.getenv("HOME")
-
 -- This is used later as the default terminal and editor to run.
 
-terminal = home .. "/.local/kitty.app/bin/kitty"
-editor = os.getenv("EDITOR") or "nvim"
-editor_cmd = terminal .. " -e " .. editor
+local terminal = home .. "/.local/kitty.app/bin/kitty"
+local editor = os.getenv("EDITOR") or "nvim"
+local editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
+local modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-	-- awful.layout.suit.floating,
 	awful.layout.suit.tile,
-	-- awful.layout.suit.tile.left,
-	-- awful.layout.suit.tile.bottom,
-	-- awful.layout.suit.tile.top,
-	-- awful.layout.suit.fair,
-	-- awful.layout.suit.fair.horizontal,
-	-- awful.layout.suit.spiral,
-	-- awful.layout.suit.spiral.dwindle,
-	-- awful.layout.suit.max,
-	-- awful.layout.suit.max.fullscreen,
-	-- awful.layout.suit.magnifier,
-	-- awful.layout.suit.corner.nw,
-	-- awful.layout.suit.corner.ne,
-	-- awful.layout.suit.corner.sw,
-	-- awful.layout.suit.corner.se,
 }
--- }}}
-
--- Polybar
 awful.spawn.with_shell(home .. "/dotfiles/start_polybar.sh")
-
--- {{{ Menu
 -- Create a launcher widget and a main menu
-myawesomemenu = {
+local myawesomemenu = {
 	{
 		"Hotkeys",
 		function()
@@ -126,11 +97,11 @@ beautiful.menu_fg_focus = "#bfe82c"
 beautiful.menu_bg_normal = "#424242"
 beautiful.menu_bg_focus = "#222222"
 
-mymainmenu = awful.menu({
+local mymainmenu = awful.menu({
 	items = {
 		{ "Awesome", myawesomemenu, beautiful.awesome_icon },
 		{ " Terminal", terminal },
-		{ " Browser", "librewolf" },
+		{ " Browser", "mullvad" },
 		{ " Files", "nautilus" },
 		{ " Lock", "xscreensaver-command -lock" },
 		{
@@ -144,25 +115,8 @@ mymainmenu = awful.menu({
 	},
 })
 
--- userwidget = wibox.widget.textbox()
+menubar.utils.terminal = terminal
 
--- userwidget:set_text("|-o-|  " .. os.getenv("USER") .. "  |-o-|")
-
--- mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
---                                      menu = mymainmenu })
-
--- Menubar configuration
-menubar.utils.terminal = terminal -- Set the terminal for applications that require it
--- }}}
-
--- Keyboard map indicator and switcher
--- mykeyboardlayout = awful.widget.keyboardlayout()
-
--- {{{ Wibar
--- Create a textclock widget
--- mytextclock = wibox.widget.textclock("| %d/%m/%y | %I:%M %p | ")
-
--- Create a wibox for each screen and add it
 local taglist_buttons = gears.table.join(
 	awful.button({}, 1, function(t)
 		t:view_only()
@@ -205,28 +159,13 @@ local tasklist_buttons = gears.table.join(
 	end)
 )
 
--- {{{ Random Wallpapers
-
--- Get the list of files from a directory. Must be all images or folders and non-empty.
-local function scanDir(directory)
-	local i, fileList, popen = 0, {}, io.popen
-	for filename in popen([[find "]] .. directory .. [[" -type f]]):lines() do
-		i = i + 1
-		fileList[i] = filename
-	end
-	return fileList
-end
-
 local function set_wallpaper(s)
 	-- Wallpaper
 	if beautiful.wallpaper then
 		local wallpaper = beautiful.wallpaper
-		-- If wallpaper is a function, call it with the screen
 		if type(wallpaper) == "function" then
 			wallpaper = wallpaper(s)
 		end
-		-- gears.wallpaper.maximized(wallpaper, s, false)
-		--gears.wallpaper.tiled(wallpaper, s, false)
 		gears.wallpaper.centered(wallpaper, s, false)
 	end
 end
@@ -238,43 +177,12 @@ local function is_fedora()
 	return result:find("Fedora") ~= nil
 end
 
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
 screen.connect_signal("property::geometry", set_wallpaper)
 
 awful.screen.connect_for_each_screen(function(s)
-	if s.index == 2 then
-	-- Set systray
-	-- s.systray = wibox.widget.systray()
-	-- s.systray.visible = true
-
-	-- Wallpaper
-
-	-- wallpaperList = scanDir(home .. "/Pictures/actual")
-
 	-- Set wallpaper
 	gears.wallpaper.maximized(home .. "/Pictures/actual/background.png", s, true)
 
-	-- Apply a random wallpaper on startup.
-	-- gears.wallpaper.maximized(wallpaperList[math.random(1, #wallpaperList)], s, true)
-
-	-- Apply a random wallpaper every changeTime seconds.
-	-- changeTime = 600
-	-- wallpaperTimer = timer({ timeout = changeTime })
-	-- wallpaperTimer:connect_signal("timeout", function()
-	-- gears.wallpaper.maximized(wallpaperList[math.random(1, #wallpaperList)], s, true)
-
-	-- stop the timer (we don't need multiple instances running at the same time)
-	-- wallpaperTimer:stop()
-
-	--restart the timer
-	-- wallpaperTimer.timeout = changeTime
-	-- wallpaperTimer:start()
-	-- end)
-
-	-- initial start when rc.lua is first run
-	-- wallpaperTimer:start()
-
-	-- Each screen has its own tag table.
 	if is_fedora() then
 		if s.index == 2 then
 			awful.tag({ "1", "2", "3", "4", "·", "·", "·", "·", "·" }, s, awful.layout.layouts[1])
@@ -288,12 +196,7 @@ awful.screen.connect_for_each_screen(function(s)
 			awful.tag({ "~", "~" }, s, awful.layout.layouts[1])
 		end
 	end
-	-- s.padding = { top = 40 }
-
-	-- Create a promptbox for each screen
 	s.mypromptbox = awful.widget.prompt()
-	-- Create an imagebox widget which will contain an icon indicating which layout we're using.
-	-- We need one layoutbox per screen.
 	s.mylayoutbox = awful.widget.layoutbox(s)
 	s.mylayoutbox:buttons(gears.table.join(
 		awful.button({}, 1, function()
@@ -309,63 +212,25 @@ awful.screen.connect_for_each_screen(function(s)
 			awful.layout.inc(-1)
 		end)
 	))
-	-- Create a taglist widget
 	s.mytaglist = awful.widget.taglist({
 		screen = s,
 		filter = awful.widget.taglist.filter.all,
 		buttons = taglist_buttons,
 	})
-
 	-- Create a tasklist widget
 	s.mytasklist = awful.widget.tasklist({
 		screen = s,
 		filter = awful.widget.tasklist.filter.currenttags,
 		buttons = tasklist_buttons,
 	})
-
-	-- Create the wibox
-	-- s.mywibox = awful.wibar({ position = "top", screen = s })
-	-- s.mywibox = awful.wibar({ position = "left", screen = s })
-
-	-- Add widgets to the wibox
-	-- s.mywibox:setup({
-	-- 	layout = wibox.layout.align.horizontal,
-	-- 	-- spacing = 20,
-	-- 	{ -- Left widgets
-	-- 		layout = wibox.layout.fixed.horizontal,
-	-- 		mylauncher,
-	-- 		userwidget,
-	-- 		s.mytaglist,
-	-- 		s.mypromptbox,
-	-- 	},
-	-- 	s.mytasklist, -- Middle widget
-	-- 	{ -- Right widgets
-	-- 		layout = wibox.layout.fixed.horizontal,
-	-- 		wibox.widget.textbox(" I "),
-	-- 		network_usage_monitor,
-	-- 		wibox.widget.textbox(" I "),
-	-- 		volume_widget(),
-	-- 		mykeyboardlayout,
-	-- 		s.systray,
-	-- 		mytextclock,
-	-- 		ww,
-	-- 		-- s.mylayoutbox,
-	-- 	},
-	-- })
-end
 end)
 
--- {{{ Mouse bindings
 root.buttons(gears.table.join(
 	awful.button({}, 3, function()
 		mymainmenu:toggle()
 	end)
-	-- ,awful.button({}, 4, awful.tag.viewnext),
-	-- awful.button({}, 5, awful.tag.viewprev)
 ))
--- }}}
 
--- {{{ Key bindings
 globalkeys = gears.table.join(
 	-- Volume control
 	awful.key({}, "XF86AudioRaiseVolume", function()
@@ -422,12 +287,6 @@ globalkeys = gears.table.join(
 		end
 	end, { description = "go back", group = "client" }),
 
-	-- lockscreen using xscreensaver
-
-	-- awful.key({"Control"}, "Super_L", function () awful.spawn("xscreensaver-command -lock") end,
-	--          {description = "lock screen", group = "system"}),
-
-	-- Standard program
 	awful.key({ modkey, "Shift" }, "f", function()
 		awful.spawn("firejail /usr/bin/librewolf")
 	end, { description = "open librewolf", group = "launcher" }),
@@ -476,7 +335,6 @@ globalkeys = gears.table.join(
 
 	awful.key({ modkey, "Control" }, "n", function()
 		local c = awful.client.restore()
-		-- Focus restored client
 		if c then
 			c:emit_signal("request::activate", "key.unminimize", { raise = true })
 		end
@@ -497,7 +355,7 @@ globalkeys = gears.table.join(
 	end, { description = "rofi", group = "launcher" })
 )
 
-clientkeys = gears.table.join(
+local clientkeys = gears.table.join(
 	awful.key({ modkey }, "f", function(c)
 		c.fullscreen = not c.fullscreen
 		c:raise()
@@ -540,8 +398,6 @@ clientkeys = gears.table.join(
 )
 
 -- Bind all key numbers to tags.
--- Be careful: we use keycodes to make it work on any keyboard layout.
--- This should map on the top row of your keyboard, usually 1 to 9.
 for i = 1, 9 do
 	globalkeys = gears.table.join(
 		globalkeys,
@@ -582,7 +438,7 @@ for i = 1, 9 do
 	)
 end
 
-clientbuttons = gears.table.join(
+local clientbuttons = gears.table.join(
 	awful.button({}, 1, function(c)
 		c:emit_signal("request::activate", "mouse_click", { raise = true })
 	end),
@@ -598,7 +454,6 @@ clientbuttons = gears.table.join(
 
 -- Set keys
 root.keys(globalkeys)
--- }}}
 
 -- {{{ Rules
 -- Rules to apply to new clients (through the "manage" signal).
@@ -621,8 +476,10 @@ awful.rules.rules = {
 	{ rule = { class = "Gnome-terminal" }, properties = { screen = 1, tag = "3" } },
 	-- Open datastudio on workspace 4, identificada por nombre no por clase
 	{ rule = { name = "datastudio" }, properties = { screen = 1, tag = "4" } },
-	{ rule = { name = "Blender" }, properties = { screen = 1, tag = "4" } },
-	{ rule = { name = "UltiMaker-Cura" }, properties = { screen = 1, tag = "5" } },
+	{ rule = { class = "Blender" }, properties = { screen = 1, tag = "4" } },
+  -- Open Cura and Burpsuite on workspace 5
+	{ rule = { class = "UltiMaker-Cura" }, properties = { screen = 1, tag = "5" } },
+  { rule = { class = "install4j-burp-StartBurp", properties = { screen = 1, tag = "5"} },
 
 	-- All clients will match this rule.
 	{
@@ -680,11 +537,7 @@ awful.rules.rules = {
 	-- Add titlebars to normal clients and dialogs
 	{ rule_any = { type = { "normal", "dialog" } }, properties = { titlebars_enabled = false } },
 
-	-- Set Firefox to always map on the tag named "2" on screen 1.
-	-- { rule = { class = "Firefox" },
-	--   properties = { screen = 1, tag = "2" } },
-}
--- }}}
+}}
 
 -- Center notifications
 for _, preset in pairs(naughty.config.presets) do
@@ -756,7 +609,7 @@ awful.spawn.with_shell("xrandr --output HDMI-A-0 --primary --left-of DisplayPort
 awful.spawn.with_shell("xset s off")
 awful.spawn.with_shell("xset -dpms")
 awful.spawn.with_shell("xset s noblank")
--- awful.spawn.with_shell("picom --config ~/.picom -b")
+awful.spawn.with_shell("picom --config ~/.picom -b")
 awful.spawn.with_shell(home .. "/dotfiles/start_tray_apps.sh")
 awful.spawn.with_shell("xscreensaver -no-splash &")
 awful.spawn.with_shell("nm-applet")
@@ -775,4 +628,3 @@ end)
 client.connect_signal("unfocus", function(c)
 	c.border_color = beautiful.border_normal
 end)
--- }}}
