f46dede54777062c5c1e5473574119665e77359f
nothingbutlucas <69118979+nothingbutlucas@users.noreply.github.com>
2024-04-01 15:29:29 -0300

    refactor(polybar): Simplify gpu percentage
    Signed-off-by: nothingbutlucas <69118979+nothingbutlucas@users.noreply.github.com>

--- polybar/gpu.sh
+++ polybar/gpu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-timeout --preserve-status --kill-after=1 2 radeontop -d - | awk '{print $27}' | grep "%" | sort -u
+timeout --preserve-status --kill-after=1 2 radeontop -d - | awk '{print $27}' | grep "%" | sort -u | awk -F'.' '{print $1 "%"}'
